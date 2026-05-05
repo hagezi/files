d067b3d51b37c8d3868ef037c7d43949b3554700
hagezi <hagezi@protonmail.com>
2026-05-05 09:06:01 +0200

    2026.05.05-09:06:00

--- gitPush.sh
+++ gitPush.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-localgit=/media/nas/git/files
 comment=$1
 if [ -z "$1" ]; then
     comment=$(date +'%Y.%m.%d-%H:%M:%S')
 fi
 
-cd $localgit || exit
 git pull
 git add .
 git commit -m "$comment"
